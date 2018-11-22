6ad821e07593ce57272601bc6e4054ac97fd8c65
Filippo <f.storniolo@hotmail.it>
2018-11-23 00:39:56 +0100

    added include in kvm.cpp. Added class HardDisk with constructor and redefinition of reading and writing methods

---
+++ frontend/HardDisk.h
@@ -0,0 +1,72 @@
+#include "IODevice.h"
+#include "../backend/DiskManager.h"
+
+
+
+
+class HardDisk : public IODevice {
+private:
+	// === Registers ===
+	uint16_t BR;	// buffer that contains data to read or to write
+	uint8_t SCR;	// registers that contains the numbers of sector to read or to write
+	uint8_t SNR;	
+	uint8_t CNH;	
+	uint8_t CNL;
+	uint8_t HND;
+	uint8_t ERR;	// contains the type of error that occured
+	uint8_t STS;	// contains the disk state 
+	uint8_t CMD;	// contains the command
+
+	uint8_t DCR;	// contains the bit that allows interrupts
+	uint8_t DAR;	// here for compatibility
+	uint8_t ASR;	// here for compatibility
+   	// indirizzi registri
+	static const io_addr BR_addr = 0x01F0;
+	static const io_addr ERR_addr = 0x01F1;
+	static const io_addr SCR_addr = 0x01F2;
+	static const io_addr SNR_addr = 0x01F3;
+	static const io_addr CNL_addr = 0x01F4;
+	static const io_addr CNH_addr =	0x01F5;
+	static const io_addr HND_addr = 0x01F6;
+	static const io_addr CMD_addr = 0x01F7;
+	static const io_addr STS_addr = 0x01F7;
+	static const io_addr DCR_addr = 0x03F6;
+	static const io_addr ASR_addr = 0x03F6;
+	static const io_addr DAR_addr = 0x03F7;
+
+
+	/*static const uint8_t FI_MASK = 1u;
+	static const uint8_t FO_MASK = 1u << 1;*/
+
+	// === Internal state ===
+	bool enabled;
+	bool interrupt_enabled;
+
+	uint8_t internal_buffer[BLOCK_SIZE_BYTE];
+	int current_position;
+
+	//bool interrupt_raised;
+
+
+private:
+	void process_cmd();
+
+	// questo metodo deve essere chiamato prima di una lettura su RBR
+	// per aggiornare il contenuto del registro RBR e STR
+	void next_RBR();
+
+	// questo metodo è chiamato per aggiornare FI in base allo stato
+	// del buffer (vuoto/non vuoto)
+	void update_FI();
+
+public:
+	HardDisk();
+
+	void write_reg_byte(io_addr addr, uint8_t val);
+	uint8_t read_reg_byte(io_addr addr);
+	void write_reg_word(io_addr addr, uint16_t val);
+	uint16_t read_reg_word(io_addr addr);
+
+	// serve al backend per pushare eventi di inserimento come handler
+	//void insert_keycode_event(uint8_t keycode);
+};
